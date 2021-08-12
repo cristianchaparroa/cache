e366b4677b3d56c355dd0f961abb70e611c3ec03
cristianchaparroa <cristian.chaparro@commure.com>
2021-08-12 15:40:43 -0500

    feat: setup the application entry point

---
+++ go.mod
@@ -0,0 +1,9 @@
+module cache
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.3
+	github.com/joho/godotenv v1.3.0
+	github.com/stretchr/testify v1.7.0
+)
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"cache/app/delivery"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	s := delivery.NewServer()
+	s.Run()
+}
