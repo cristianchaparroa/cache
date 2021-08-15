6bad4bb8ce5418d0d490eb6beabb32122f95de1a
cristianchaparroa <cristian.chaparro@commure.com>
2021-08-15 13:22:41 -0500

    feat: read the file from configuration

--- app/conf/config.go
+++ app/conf/config.go
@@ -2,6 +2,7 @@ package conf
 
 import (
 	"cache/core"
+	"errors"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
@@ -13,24 +14,63 @@ func init() {
 	core.CheckInjection(err, "newConfig")
 }
 
+const (
+	OlderFistEvictionPolicy   = "OLDEST_FIRST"
+	NewestFirstEvictionPolicy = "NEWEST_FIRST"
+	RejectEvictionPolicy      = "REJECT"
+)
+
 type Config struct {
 	Slots  int    `yaml:"slots"`
 	TTL    int    `yaml:"ttl"`
 	Policy string `yaml:"policy"`
 }
 
-func newConfig() *Config {
-	file := os.Getenv("PATH_ENV")
+func newConfig() (*Config, error) {
+	file := os.Getenv("CONFIG")
 	yamlFile, err := ioutil.ReadFile(file)
 	if err != nil {
 		log.Printf("yamlFile.Get err   #%v ", err)
+		return nil, nil
 	}
 
 	var c Config
-	err = yaml.Unmarshal(yamlFile, c)
+	err = yaml.Unmarshal(yamlFile, &c)
 	if err != nil {
 		log.Fatalf("Unmarshal: %v", err)
+		return nil, err
 	}
 
-	return &c
+	err = c.IsValid()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
+
+func (c *Config) IsValid() error {
+	if c.Slots <= 0 {
+		return errors.New("the number of slots is negative")
+	}
+
+	if c.TTL <= 0 {
+		return errors.New("the TTL value is negative")
+	}
+
+	validPolicies := []string{OlderFistEvictionPolicy, NewestFirstEvictionPolicy, RejectEvictionPolicy}
+
+	isValidPolicy := false
+	for _, policy := range validPolicies {
+		if policy == c.Policy {
+			isValidPolicy = true
+		}
+	}
+
+	if !isValidPolicy {
+		return errors.New("policy not expected")
+	}
+
+	return nil
 }
