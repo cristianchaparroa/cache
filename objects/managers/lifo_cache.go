d60bbaf2db3aa71b0a54a249c905af3005d1f556
cristianchaparroa <cristian.chaparro@commure.com>
2021-08-15 18:09:52 -0500

    test: add testing for lifo cache

--- objects/managers/lifo_cache.go
+++ objects/managers/lifo_cache.go
@@ -4,15 +4,16 @@ import (
 	"cache/app/datasources"
 	"cache/objects"
 	"cache/objects/ports"
-	"errors"
 )
 
 type LIFOCache struct {
-	storage datasources.Storage
+	*baseCache
 }
 
 func NewLIFOCache(storage datasources.Storage) ports.CacheManager {
-	return &LIFOCache{storage: storage}
+	return &LIFOCache{
+		baseCache: &baseCache{storage: storage},
+	}
 }
 
 func (c *LIFOCache) Add(key string, o *objects.Object) bool {
@@ -21,24 +22,12 @@ func (c *LIFOCache) Add(key string, o *objects.Object) bool {
 		return c.storage.Add(key, o)
 	}
 
-	back := c.storage.Back()
-	_, isDeleted := c.storage.Delete(back.KeyToString())
+	oldest := c.storage.Front()
+	_, isDeleted := c.storage.Delete(oldest.KeyToString())
 
-	if isDeleted {
+	if !isDeleted {
 		return false
 	}
 
 	return c.storage.Add(key, o)
-}
-
-func (c *LIFOCache) Get(key string) (*objects.Object, error) {
-	panic("implement me")
-}
-
-func (c *LIFOCache) Delete(key string) (*objects.Object, error) {
-	obj, bool := c.storage.Delete(key)
-	if bool {
-		return obj.(*objects.Object), nil
-	}
-	return nil, errors.New("object not found")
 }
